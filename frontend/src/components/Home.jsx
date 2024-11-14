3c4cae2997011fb7ea35c5935d65829ef9b49969
AdityaWaradkar <adityawaradkar2004@gmail.com>
2024-11-14 20:22:05 +0530

    finished with the project on local machine

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,12 +5,10 @@ import TodoComponent from "./Todo";
 const Home = () => {
   return (
     <div className="min-h-screen bg-gray-100">
-      {" "}
       {/* Full height screen and background color */}
       <Navbar /> {/* Include the Navbar in the Home page */}
       <div className="flex flex-col items-center justify-center p-6 sm:p-12 md:p-16">
         <div className="w-full max-w-5xl">
-          {" "}
           {/* Make sure TodoComponent doesn't stretch too wide */}
           <TodoComponent />
         </div>
